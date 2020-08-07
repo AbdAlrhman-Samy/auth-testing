779fed0987d390126a095088eac41de8a9fc2a0d
abdalrhman-samy <abdalrhman.samy@outlook.com>
2020-08-07 17:45:13 +0200

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAjBfmM1G-Fy9hkUgTOvcoVIdLT-X_7h30",
+    authDomain: "testing-auth236.firebaseapp.com",
+    databaseURL: "https://testing-auth236.firebaseio.com",
+    projectId: "testing-auth236",
+    storageBucket: "testing-auth236.appspot.com",
+    messagingSenderId: "112041743062",
+    appId: "1:112041743062:web:108035d518dcd55634af77"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
